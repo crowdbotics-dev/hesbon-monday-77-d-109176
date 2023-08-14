dc92c6d2f9565d0552847da269b92cdb6c95be29
Crowdbotics <engineering@crowdbotics.com>
2023-08-14 18:10:13 +0000

    Update store.

--- store/hesbonmondaydAPI/api.js
+++ store/hesbonmondaydAPI/api.js
@@ -8,6 +8,24 @@ function api_docs_schema_retrieve(payload) {
     params: { lang: payload.lang }
   })
 }
+function api_v1_article_list(payload) {
+  return hesbonmondaydAPI.get(`/api/v1/article/`)
+}
+function api_v1_article_create(payload) {
+  return hesbonmondaydAPI.post(`/api/v1/article/`, payload)
+}
+function api_v1_article_retrieve(payload) {
+  return hesbonmondaydAPI.get(`/api/v1/article/${payload.id}/`)
+}
+function api_v1_article_update(payload) {
+  return hesbonmondaydAPI.put(`/api/v1/article/${payload.id}/`, payload)
+}
+function api_v1_article_partial_update(payload) {
+  return hesbonmondaydAPI.patch(`/api/v1/article/${payload.id}/`, payload)
+}
+function api_v1_article_destroy(payload) {
+  return hesbonmondaydAPI.delete(`/api/v1/article/${payload.id}/`)
+}
 function api_v1_login_create(payload) {
   return hesbonmondaydAPI.post(`/api/v1/login/`, payload)
 }
@@ -49,6 +67,12 @@ function rest_auth_user_partial_update(payload) {
 }
 export const apiService = {
   api_docs_schema_retrieve,
+  api_v1_article_list,
+  api_v1_article_create,
+  api_v1_article_retrieve,
+  api_v1_article_update,
+  api_v1_article_partial_update,
+  api_v1_article_destroy,
   api_v1_login_create,
   api_v1_signup_create,
   rest_auth_login_create,
